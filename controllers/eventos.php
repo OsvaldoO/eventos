9fff3a6b3f9e90eb5ff12021e65bb8774f7fa8ae
OsvaldoO <osvaldo.lp.5889@gmail.com>
2014-03-24 11:06:53 -0600

    Conectando con Base de datos

--- controllers/eventos.php
+++ controllers/eventos.php
@@ -1,22 +1,62 @@
 <?php
 	include('views/events.php');
+
+	class Bd{
+		private $con;
+		private $db;
+
+		private function conectar(){
+			$this->con = mysql_connect ( 'localhost', 'oosvaldo', 'oOnzO' );
+			$this->db = mysql_select_db( 'eventos', $this->con ) or die ("Error Base de datos");
+		}
+
+		public function solicitud($sql){
+			$this->conectar();
+			$res = mysql_query( $sql );
+			mysql_close($this->con);
+			return ($res)?$res:NULL;
+		}
+	}
+
 	class Eventos{
-		public function inicio(){
+		public function listar(){
+			$bd = new Bd();
+			$sql = 'SELECT name,fecha,descripcion,img FROM eventos';
+			$res = $bd->solicitud($sql);
+			$eventos = array();
+			$i = 0;
+			while ($record = mysql_fetch_array($res)) {
+				$eventos[$i]['name'] = $record['name'];
+				$eventos[$i]['fecha'] = $record['fecha'];
+				$eventos[$i]['descripcion'] = $record['descripcion'];
+				$eventos[$i]['img'] = $record['img'];
+				$i++;
+			}
 			template_header('Eventos');
-			eventos_index();
+			eventos_index($eventos);
 			template_footer();
 		}
 
-		public function listar(){
+		public function ver($id = 1){
+			$bd = new Bd();
+			$sql = 'SELECT name,fecha,descripcion,img FROM eventos WHERE id='.$id;
+			$res = $bd->solicitud($sql);
+			$evento = array();
+			while ($record = mysql_fetch_array($res)) {
+				$evento['name'] = $record['name'];
+				$evento['fecha'] = $record['fecha'];
+				$evento['descripcion'] = $record['descripcion'];
+				$evento['img'] = $record['img'];
+			}
 			template_header('Eventos');
-			evento_show();
+			evento_show($evento);
 			template_footer();
 		}
 	}
-	
+
 	$evento = new Eventos();
 	if(count($ACTION) > 1){
 		$evento->$ACTION[1]();
 	}
-	else $evento->inicio();
+	else $evento->listar();
 ?>
--- views/events.php
+++ views/events.php
@@ -1,206 +1,49 @@
 <?php 
-function eventos_index(){
-	?>
-			<script type="text/javascript" src="<?php echo URL_ROOT?>/js/easyTooltip.js"></script>
-		<script type="text/javascript">
-			$(window).load(function(){
-				$("a[data-gal^='prettyVideo']").prettyPhoto({animation_speed:'normal',theme:'facebook',slideshow:false, autoplay_slideshow: false});
-				$("a[data-gal^='prettyVideo_1']").prettyPhoto({animation_speed:'normal',theme:'facebook',slideshow:false, autoplay_slideshow: false});
-				$("a[data-gal^='prettyVideo_2']").prettyPhoto({animation_speed:'normal',theme:'facebook',slideshow:false, autoplay_slideshow: false});
-				$("a[data-gal^='prettyVideo_3']").prettyPhoto({animation_speed:'normal',theme:'facebook',slideshow:false, autoplay_slideshow: false});
-				$("a[data-gal^='prettyVideo_4']").prettyPhoto({animation_speed:'normal',theme:'facebook',slideshow:false, autoplay_slideshow: false});
-				$("a[data-gal^='prettyVideo_5']").prettyPhoto({animation_speed:'normal',theme:'facebook',slideshow:false, autoplay_slideshow: false});
-			}); 
-		</script>	
-<!--==============================header=================================-->
-
+function eventos_index($eventos){
+?>
 <!--==============================content================================-->
-			<section id="content"><div class="ic">More Website Templates @ TemplateMonster.com. November 21, 2011!</div>
+<section id="content"><div class="ic">More Website Templates @ TemplateMonster.com. November 21, 2011!</div>
 				<div class="main">
 					<div class="content-padding-2">
 						<div class="container_12">
 							<div class="wrapper">
-								<div class="grid_12">
-									<div class="padding-grid-1">
-										<h3 class="letter">Our <strong>Music Videos</strong></h3>
-									</div>
-									<div class="wrapper indent-bot1">
-										<article class="grid_4 alpha">
-											<div class="padding-grid-2">
-												<div class="wrapper p2">
-													<figure class="style-img-2 fleft"><a class="lightbox-image" href="video/video_AS3.swf?width=495&amp;height=275&amp;fileVideo=intro06.flv" data-gal="prettyVideo[prettyVideo]"><img src="<?php echo URL_ROOT?>/images/page3-img1.jpg"  alt=""></a></figure>
-												</div>
-												<p class="prev-indent-bot2">Temporibus autem quibusdam</p>
-												<a class="link-2" href="#"></a>
-											</div>
-										</article>
-										<article class="grid_4 alpha">
-											<div class="padding-grid-3">
-												<div class="wrapper p2">
-													<figure class="style-img-2 fleft"><a class="lightbox-image" href="video/video_AS3.swf?width=495&amp;height=275&amp;fileVideo=intro06.flv" data-gal="prettyVideo[prettyVideo_1]"><img src="<?php echo URL_ROOT?>/images/page3-img2.jpg"  alt=""></a></figure>
-												</div>
-												<p class="prev-indent-bot2">Temporibus autem quibusdam</p>
-												<a class="link-2 link-style1" href="#"></a>
-											</div>
-										</article>
-										<article class="grid_4 alpha">
-											<div class="padding-grid-2">
-												<div class="wrapper p2">
-													<figure class="style-img-2 fleft"><a class="lightbox-image" href="video/video_AS3.swf?width=495&amp;height=275&amp;fileVideo=intro06.flv" data-gal="prettyVideo[prettyVideo_2]"><img src="<?php echo URL_ROOT?>/images/page3-img3.jpg"  alt=""></a></figure>
-												</div>
-												<p class="prev-indent-bot2">Temporibus autem quibusdam</p>
-												<a class="link-2 link-style2" href="#"></a>
-											</div>
-										</article>
-									</div>
-									<div class="wrapper">
-										<article class="grid_4 alpha">
-											<div class="padding-grid-2">
-												<div class="wrapper p2">
-													<figure class="style-img-2 fleft"><a class="lightbox-image" href="video/video_AS3.swf?width=495&amp;height=275&amp;fileVideo=intro06.flv" data-gal="prettyVideo[prettyVideo_3]"><img src="<?php echo URL_ROOT?>/images/page3-img4.jpg"  alt=""></a></figure>
-												</div>
-												<p class="prev-indent-bot2">Temporibus autem quibusdam</p>
-												<a class="link-2" href="#"></a>
-											</div>
-										</article>
-										<article class="grid_4 alpha">
-											<div class="padding-grid-3">
-												<div class="wrapper p2">
-													<figure class="style-img-2 fleft"><a class="lightbox-image" href="video/video_AS3.swf?width=495&amp;height=275&amp;fileVideo=intro06.flv" data-gal="prettyVideo[prettyVideo_4]"><img src="<?php echo URL_ROOT?>/images/page3-img5.jpg"  alt=""></a></figure>
-												</div>
-												<p class="prev-indent-bot2">Temporibus autem quibusdam</p>
-												<a class="link-2 link-style1" href="#"></a>
-											</div>
-										</article>
-										<article class="grid_4 alpha">
-											<div class="padding-grid-2">
-												<div class="wrapper p2">
-													<figure class="style-img-2 fleft"><a class="lightbox-image" href="video/video_AS3.swf?width=495&amp;height=275&amp;fileVideo=intro06.flv" data-gal="prettyVideo[prettyVideo_5]"><img src="<?php echo URL_ROOT?>/images/page3-img6.jpg"  alt=""></a></figure>
-												</div>
-												<p class="prev-indent-bot2">Temporibus autem quibusdam</p>
-												<a class="link-2 link-style2" href="#"></a>
-											</div>
-										</article>
-									</div>
-								</div>
-							</div>
+							<?php
+							if($eventos){
+								foreach ($eventos as $evento) {
+									echo $evento['name'];
+									echo $evento['fecha'];
+									echo $evento['descripcion'];
+									echo $evento['img'];
+								}
+							}
+							else echo '<h1>No hay Eventos</h1>';
+							 ?>
+						 </div>
 						</div>
 					</div>
 				</div>
-			<div class="block"></div>
+				<div class="block"></div>
 			</section>
 		</div>
+<!--==============================footer=================================-->
 <?php
 }
 
-function evento_show(){
+function evento_show($evento){
 ?>
-	<!--==============================header=================================-->
-
 <!--==============================content================================-->
-			<section id="content"><div class="ic">More Website Templates @ TemplateMonster.com. November 21, 2011!</div>
+<section id="content"><div class="ic">More Website Templates @ TemplateMonster.com. November 21, 2011!</div>
 				<div class="main">
 					<div class="content-padding-2">
 						<div class="container_12">
 							<div class="wrapper">
-								<div class="grid_8">
-									<div class="padding-grid-1">
-										<h3>Tour <strong>Dates</strong></h3>
-									</div>	
-									<div class="wrapper">
-										<article class="grid_4 alpha">
-											<div class="padding-grid-1">
-												<div class="wrapper img-indent-bot2">
-													<time class="time time-stule-2" datetime="2011-11-09"> <strong class="text-3">09</strong><strong class="text-4">nov</strong></time>
-													<div class="extra-wrap">
-														<h4 class="indent-top"><a href="#">New York</a></h4>
-														Lorem ipsum dolor consctetur 
-													</div>
-												</div>
-												<div class="wrapper img-indent-bot2">
-													<time class="time time-stule-2" datetime="2011-11-05"> <strong class="text-3">05</strong><strong class="text-4">nov</strong></time>
-													<div class="extra-wrap">
-														<h4 class="indent-top"><a href="#">Los Angeles</a></h4>
-														Adipisicing elitdo esmod tempor
-													</div>
-												</div>
-												<div class="wrapper img-indent-bot2">
-													<time class="time time-stule-2" datetime="2011-11-01"> <strong class="text-3">01</strong><strong class="text-4">nov</strong></time>
-													<div class="extra-wrap">
-														<h4 class="indent-top"><a href="#">San Diego</a></h4>
-														Lorem ipsum dolor consctetur 
-													</div>
-												</div>
-												<div class="wrapper img-indent-bot2">
-													<time class="time time-stule-2" datetime="2011-10-31"> <strong class="text-3">31</strong><strong class="text-4">oct</strong></time>
-													<div class="extra-wrap">
-														<h4 class="indent-top"><a href="#">San Francisco</a></h4>
-														Lorem ipsum dolor consctetur 
-													</div>
-												</div>
-												<div class="wrapper img-indent-bot2">
-													<time class="time time-stule-2" datetime="2011-10-29"> <strong class="text-3">29</strong><strong class="text-4">oct</strong></time>
-													<div class="extra-wrap">
-														<h4 class="indent-top"><a href="#">Washington</a></h4>
-														Adipisicing elitdo esmod tempor 
-													</div>
-												</div>
-												<div class="wrapper">
-													<time class="time time-stule-2" datetime="2011-10-24"> <strong class="text-3">24</strong><strong class="text-4">oct</strong></time>
-													<div class="extra-wrap">
-														<h4 class="indent-top"><a href="#">Las Vegas</a></h4>
-														Lorem ipsum dolor consctetur  
-													</div>
-												</div>
-											</div>
-										</article>
-										<article class="grid_4 omega">
-											<div class="padding-grid-4">
-												<div class="margin-bot1">
-													<span class="text-width">Tickets:</span> <span class="inline"><a class="link" href="#">http://demolink.org/Okedo</a></span>
-													<span class="text-width">Telephone:</span>  <span class="color-1 inline">+1 959 603 6035</span>
-												</div>
-												<div class="margin-bot1">
-													<span class="text-width">Tickets:</span> <span class="inline"><a class="link" href="#">http://demolink.org/Okedo</a></span>
-													<span class="text-width">Telephone:</span>  <span class="color-1 inline">+1 959 603 6035</span>
-												</div>
-												<div class="margin-bot1">
-													<span class="text-width">Tickets:</span> <span class="inline"><a class="link" href="#">http://demolink.org/Okedo</a></span>
-													<span class="text-width">Telephone:</span>  <span class="color-1 inline">+1 959 603 6035</span>
-												</div>
-												<div class="margin-bot1">
-													<span class="text-width">Tickets:</span> <span class="inline"><a class="link" href="#">http://demolink.org/Okedo</a></span>
-													<span class="text-width">Telephone:</span>  <span class="color-1 inline">+1 959 603 6035</span>
-												</div>
-												<div class="margin-bot1">
-													<span class="text-width">Tickets:</span> <span class="inline"><a class="link" href="#">http://demolink.org/Okedo</a></span>
-													<span class="text-width">Telephone:</span>  <span class="color-1 inline">+1 959 603 6035</span>
-												</div>
-												<span class="text-width">Tickets:</span> <span class="inline"><a class="link" href="#">http://demolink.org/Okedo</a></span>
-												<span class="text-width">Telephone:</span>  <span class="color-1 inline">+1 959 603 6035</span>
-											</div>
-										</article>
-									</div>
-								</div>
-								<article class="grid_4 alpha">
-									<div class="padding-grid-3">
-										<h3>Past <strong>Tours</strong></h3>
-										<ul class="list-3">
-											<li><a href="#">Lorem ipsum dolor consctetur</a></li>
-											<li><a href="#">Adipisicing eusmod tempor incididunt</a></li>
-											<li><a href="#">Dolore magna aliqua enim ad minim</a></li>
-											<li><a href="#">Quis nostrud exercitation ullamco laboris</a></li>
-											<li><a href="#">Nisi ut aliquip ex ea commodo</a></li>
-											<li><a href="#">Duis aute irure dolor in reprehenderit</a></li>
-											<li><a href="#">Coluptate velit esse cillum</a></li>
-											<li><a href="#">Dolore eu fugiat nulla pariatur</a></li>
-											<li><a href="#">Excepteur sint occaecat cupidatat</a></li>
-											<li><a href="#">Sunt in culpa qui officia</a></li>
-											<li><a href="#">Deserunt mollit anim id est laborum</a></li>
-										</ul>
-									</div>
-								</article>	
-							</div>
+									<?php
+									echo $evento['name'];
+									echo $evento['fecha'];
+									echo $evento['descripcion'];
+									echo $evento['img'];
+									?>
+						 	</div>
 						</div>
 					</div>
 				</div>
